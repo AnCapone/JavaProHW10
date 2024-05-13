265a04dc72c74014578e31b1b53cbcb8fb291afb
AnCapone <okai.kot44@gmail.com>
2024-05-13 20:17:16 +0300

    fully implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        try {
+            int sum = new ArrayValueCalculator().doCalc(new String[][]{
+                    {"1", "2", "3", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "10", "11", "12"},
+                    {"13", "14", "15", "16"}
+            });
+            System.out.println(sum);
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
 
 
     }
